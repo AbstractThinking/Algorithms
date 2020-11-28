75f1aad7137a7e96f924292812d79273fbe182fb
AldrinTaylor <48501934+AldrinTaylor@users.noreply.github.com>
2020-11-28 17:02:23 -0300

    Add files via upload

--- algorithms/probability.py
+++ algorithms/probability.py
@@ -42,8 +42,44 @@ def normal_pdf(x, mu=0, sigma=1):
 def normal_cdf(x,  mu=0, sigma=1):
     return (1 + math.erf((x-mu)/(sigma*2**0.5))) / 2;
 
-def bernoulli_trial(p):
-    return 1 if random.random() < p else 0;
+def inverse_normal_cdf(prob, mu, sigma, precision=0.001):
+    is_not_std = mu != 0 and sigma != 1; 
+    transform_z_to_x = lambda z, mu, sigma:  mu + sigma * z; 
+    
+    if is_not_std:
+        z = inverse_normal_cdf(prob, 0, 1);
+        x = transform_z_to_x(z, 0, 1);
 
-def binomial_trial(p, n):
-    return sum([bernoulli_trial(p) for i in range(n)]);
+        return x;
+
+    low_z = -10.0;
+    high_z = 10.0;
+
+    while high_z - low_z > precision:
+        mid_z = (low_z + high_z) / 2;
+        mid_p = normal_cdf(mid_z);
+
+        if mid_p < prob:
+            low_z = mid_z;
+        elif mid_p > prob:
+            high_z = mid_z;
+        else:
+            break;
+
+    return mid_z;
+
+def bernoulli_trial(prob):
+    return 1 if random.random() < prob else 0;
+
+def binomial_trial(n, prob):
+    return sum([bernoulli_trial(prob) for i in range(n)]);
+
+def normal_bounds(prob, mu=0, sigma=1):
+    tail_prob = complement(prob) / 2;
+    lower_bound = inverse_normal_cdf(tail_prob, mu, sigma);
+    upper_bound = inverse_normal_cdf(complement(tail_prob), mu, sigma);
+
+    return lower_bound, upper_bound;
+
+def normal_probability(a, b, mu=0, sigma=1):
+    return abs(normal_cdf(a, mu, sigma) - normal_cdf(b, mu, sigma))
